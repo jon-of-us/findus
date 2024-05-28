776e7a870a2ff35e2895d21e25a56f97cac4b8fc
Phytoplancton <jonas-ullmann@web.de>
2024-05-28 14:39:35 +0200

    add new test case

--- test/main.go
+++ test/main.go
@@ -1,46 +1,49 @@
 package main
 
-import (
-	"os"
+import "fmt"
 
-	"github.com/gdamore/tcell/v2"
-)
-
-func main() {
-
-	s, err := tcell.NewScreen()
-	if err != nil {
-		panic("new sreen error")
-	}
-
-	err = s.Init()
-	if err != nil {
-		panic("init error")
-	}
-
-	s.PollEvent()
-	s.PollEvent()
-
-	var style tcell.Style
-
-	style = style.Foreground(tcell.ColorRed).Background(tcell.ColorDefault).Reverse(false)
-	s.SetContent(0, 0, 'x', nil, style)
-	s.Show()
-
-	s.PollEvent()
-
-	style = style.Background(tcell.ColorDefault).Foreground(tcell.ColorDefault).Reverse(true)
-	s.SetContent(0, 0, ' ', nil, style)
-	s.Show()
-
-	s.PollEvent()
-
-	s.Fini()
-	os.Exit(0)
+type inner struct {
+	a int 
+	b int
+}
+type outer struct {
+	c int 
+	inner
+}
+type hasInner interface {
+	getInner() *inner
+}
+var _ hasInner = &outer{}
+func (o *outer) getInner() *inner {
+	return &o.inner
 }
 
-//style = style.Background(tcell.ColorRed).Foreground(tcell.ColorReset).Reverse(true)
-//case highlighted && !inverted && char != '.':
-//style = style.Foreground(tcell.ColorRed).Background(tcell.ColorDefault).Reverse(false)
-//default:
-//style = style.Background(tcell.ColorDefault).Foreground(tcell.ColorDefault).Reverse(inverted)
+
+func newOuter(options ...func(*outer)) outer {
+	o := outer{}
+	for _, opt := range options {
+		opt(&o)
+	}
+	return o
+}
+func withC(c int) func(*outer) {
+	return func(o *outer) {
+		o.c = c
+	}
+}
+func withA[T hasInner](a int) func(T) {
+	return func(t T) {
+		t.getInner().a = a
+	}
+}
+func withB[T hasInner](b int) func(T) {
+	return func(t T) {
+		t.getInner().b = b
+	}
+}
+
+func main() {
+	o := newOuter(withC(1), withA[*outer](2), withB[*outer](3))
+	fmt.Println("jo")
+	fmt.Println(o)
+}
