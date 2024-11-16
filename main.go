297d61e88bbe21057e01c06dc9ee02946ef9b7bd
Phytoplancton <jonas-ullmann@web.de>
2024-11-16 18:07:58 +0100

    clear main branch

--- main.go
+++ main.go
@@ -1,62 +1,7 @@
 package main
 
-import (
-	"findus/gui/components"
-	"findus/gui/screen"
-
-	"github.com/gdamore/tcell/v2"
-)
+import "fmt"
 
 func main() {
-	start()
-}
-
-func start() {
-	mainScreen := screen.Init()
-	var width, height int
-
-	empty1 := components.NewEmpty()
-	empty1.Props().SetMustRender(true).SetMinWidth(20)
-
-	empty2 := components.NewEmpty()
-	empty2.Props().SetMinWidth(25)
-
-	empty3 := components.NewEmpty()
-	empty3.Props().SetMinWidth(30).SetMustRender(true)
-	
-	var app components.Component = components.NewColumn(
-		[]components.Component{
-			components.NewLine(),
-			components.NewRow(
-				[]components.Component{
-					components.NewOutline(
-						empty1,
-					),
-					components.NewOutline(
-						empty2,
-					),
-					components.NewOutline(
-						empty3,
-					),
-				},
-			),
-		},
-	)
-	//eventLoop
-	for {
-		event := mainScreen.PollEvent()
-		switch ev := event.(type) { // type switch https://go.dev/tour/methods/16
-			case *tcell.EventResize:
-				width, height = ev.Size()
-			case *tcell.EventKey:
-				switch ev.Key() {
-					case tcell.KeyEscape:
-						screen.Quit(mainScreen)
-				}
-		}
-		mainScreen.Fill(' ', tcell.StyleDefault)
-		mainScreen.Clear()
-		app.Render(screen.NewRenderBox(mainScreen, 0, 0, width, height))
-		mainScreen.Show()
-	}
+	fmt.Println("Hello, World!")
 }
